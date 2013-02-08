c6f06dd7e7aa26d2f35f39114101a894fdd4d934
Brett Cannon <brett@python.org>
2013-02-08 11:17:14 -0500

    Factor out common function code to a generating function

--- defusedxml/common.py
+++ defusedxml/common.py
@@ -42,3 +42,36 @@ def _wire_module(srcmod, dstmodname):
             continue
         value = getattr(srcmod, name)
         setattr(dstmod, name, value)
+
+def _generate_etree_functions(DefusedXMLParser, _TreeBuilder,
+            _IterParseIterator, _parse, _iterparse):
+    def parse(source, parser=None, forbid_dtd=False, forbid_entities=True):
+        if parser is None:
+            parser = DefusedXMLParser(target=_TreeBuilder(),
+                                      forbid_dtd=forbid_dtd,
+                                      forbid_entities=forbid_entities)
+        return _parse(source, parser)
+
+    def iterparse(source, events=None, parser=None, forbid_dtd=False,
+                forbid_entities=True):
+      if PY3:
+          close_source = False
+          if not hasattr(source, "read"):
+              source = open(source, "rb")
+              close_source = True
+          if not parser:
+              parser = DefusedXMLParser(target=_TreeBuilder())
+          return _IterParseIterator(source, events, parser, close_source)
+      else:
+          if parser is None:
+              parser = DefusedXMLParser(target=_TreeBuilder())
+          return _iterparse(source, events, parser)
+
+    def fromstring(text, forbid_dtd=False, forbid_entities=True):
+        parser = DefusedXMLParser(target=_TreeBuilder(),
+                                  forbid_dtd=forbid_dtd,
+                                  forbid_entities=forbid_entities)
+        parser.feed(text)
+        return parser.close()
+
+    return parse, iterparse, fromstring
