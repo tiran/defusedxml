98e72918b67a7775df0c783d6828de17e42a5062
Christian Heimes <christian@python.org>
2013-02-17 15:31:02 +0100

    Add experimental monkey patching

--- defusedxml/__init__.py
+++ defusedxml/__init__.py
@@ -8,16 +8,35 @@
 from __future__ import print_function, absolute_import
 
 from .common import (DefusedXmlException, DTDForbidden, EntitiesForbidden,
-                     ExternalReferenceForbidden, NotSupportedError)
+                     ExternalReferenceForbidden, NotSupportedError,
+                     _apply_defusing)
 
-#from . import cElementTree
-#from . import ElementTree
-#from . import minidom
-#from . import pulldom
-#from . import sax
-#from . import xmlrpc
-#from . import expatbuilder
-#from . import expatreader
+def defuse_stdlib():
+    """Monkey patch and defuse all stdlib packages
+
+    :warning: It's an HIGHLY EXPERIMETNAL and hardly tested feature.
+    """
+    defused = {}
+
+    from . import cElementTree
+    from . import ElementTree
+    from . import minidom
+    from . import pulldom
+    from . import sax
+    from . import expatbuilder
+    from . import expatreader
+    from . import xmlrpc
+
+    xmlrpc.monkey_patch()
+    defused[xmlrpc] = None
+
+    for defused_mod in [cElementTree, ElementTree, minidom, pulldom, sax,
+                        expatbuilder, expatreader]:
+        stdlib_mod = _apply_defusing(defused_mod)
+        defused[defused_mod] = stdlib_mod
+
+    return defused
+
 
 __version__ = "0.3dev"
 
