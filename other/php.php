f0e0a6fec15339cff113497fe65fe7ec11691c76
Christian Heimes <christian@python.org>
2013-02-25 00:49:17 +0100

    PHP's libxml_disable_entity_loader() prevents entity expansion

--- other/php.php
+++ other/php.php
@@ -5,6 +5,9 @@
 // $options = LIBXML_NONET;
 $options = LIBXML_NOENT;
 
+/* LIBXML_NOENT doesn't have any effect but
+   libxml_disable_entity_loader(true) works */
+
 $xml = simplexml_load_file($argv[1], "SimpleXMLElement", $options);
 $data = (string)$xml;
 echo strlen($data);
