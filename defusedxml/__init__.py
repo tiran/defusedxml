f1895106a5d64d1effa5d609d56b6fdc39693b4b
Jon Dufresne <jon.dufresne@gmail.com>
2018-04-04 21:39:54 -0700

    Use https:// URLs where available

--- defusedxml/__init__.py
+++ defusedxml/__init__.py
@@ -2,7 +2,7 @@
 #
 # Copyright (c) 2013 by Christian Heimes <christian@python.org>
 # Licensed to PSF under a Contributor Agreement.
-# See http://www.python.org/psf/license for licensing details.
+# See https://www.python.org/psf/license for licensing details.
 """Defuse XML bomb denial of service vulnerabilities
 """
 from __future__ import print_function, absolute_import
--- defusedxml/cElementTree.py
+++ defusedxml/cElementTree.py
@@ -2,7 +2,7 @@
 #
 # Copyright (c) 2013 by Christian Heimes <christian@python.org>
 # Licensed to PSF under a Contributor Agreement.
-# See http://www.python.org/psf/license for licensing details.
+# See https://www.python.org/psf/license for licensing details.
 """Defused xml.etree.cElementTree
 """
 from __future__ import absolute_import
