bcb28823bcd8553d1470a13d6dc2cc700e17911d
Davide Brunato <brunato@sissa.it>
2019-05-08 08:21:51 +0200

    Fix xml.etree.ElementTree module import for Python 3

--- defusedxml/ElementTree.py
+++ defusedxml/ElementTree.py
@@ -9,19 +9,45 @@ from __future__ import print_function, absolute_import
 
 import sys
 import warnings
-from xml.etree.ElementTree import TreeBuilder as _TreeBuilder
-from xml.etree.ElementTree import parse as _parse
-from xml.etree.ElementTree import tostring
 
 from .common import PY3
 
 if PY3:
     import importlib
+
+    def _get_py3_cls():
+        """Python 3.3 hides the pure Python code but defusedxml requires it.
+
+        The code is based on test.support.import_fresh_module().
+        """
+        pymodname = "xml.etree.ElementTree"
+        cmodname = "_elementtree"
+
+        pymod = sys.modules.pop(pymodname, None)
+        cmod = sys.modules.pop(cmodname, None)
+
+        sys.modules[cmodname] = None
+        pure_pymod = importlib.import_module(pymodname)
+        if cmod is not None:
+            sys.modules[cmodname] = cmod
+        else:
+            sys.modules.pop(cmodname)
+        if pymod is not None:
+            sys.modules[pymodname] = pymod
+        else:
+            sys.modules.pop(pymodname)
+
+        return pure_pymod.XMLParser, pure_pymod.iterparse, pure_pymod.ParseError
+
+    _XMLParser, _iterparse, ParseError = _get_py3_cls()
 else:
     from xml.etree.ElementTree import XMLParser as _XMLParser
     from xml.etree.ElementTree import iterparse as _iterparse
     from xml.etree.ElementTree import ParseError
 
+from xml.etree.ElementTree import TreeBuilder as _TreeBuilder
+from xml.etree.ElementTree import parse as _parse
+from xml.etree.ElementTree import tostring
 
 from .common import (
     DTDForbidden,
@@ -31,36 +57,6 @@ from .common import (
 )
 
 __origin__ = "xml.etree.ElementTree"
-
-
-def _get_py3_cls():
-    """Python 3.3 hides the pure Python code but defusedxml requires it.
-
-    The code is based on test.support.import_fresh_module().
-    """
-    pymodname = "xml.etree.ElementTree"
-    cmodname = "_elementtree"
-
-    pymod = sys.modules.pop(pymodname, None)
-    cmod = sys.modules.pop(cmodname, None)
-
-    sys.modules[cmodname] = None
-    pure_pymod = importlib.import_module(pymodname)
-    if cmod is not None:
-        sys.modules[cmodname] = cmod
-    else:
-        sys.modules.pop(cmodname)
-    sys.modules[pymodname] = pymod
-
-    _XMLParser = pure_pymod.XMLParser
-    _iterparse = pure_pymod.iterparse
-    ParseError = pure_pymod.ParseError
-
-    return _XMLParser, _iterparse, ParseError
-
-
-if PY3:
-    _XMLParser, _iterparse, ParseError = _get_py3_cls()
 
 _sentinel = object()
 
